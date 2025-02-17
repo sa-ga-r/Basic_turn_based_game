e8d68770ca3a59fd254b5a7ffe226309de9079f4
sa-ga-r <116742231+sa-ga-r@users.noreply.github.com>
2025-02-17 14:57:12 +0000

    update

--- button_fighter/src/App.js
+++ button_fighter/src/App.js
@@ -5,20 +5,20 @@ import react, {useState, useEffect} from 'react';
 function App() {
   const [enemyhealth, setenemyHealth] = useState(100);
   const [playerhealth, setplayerHealth] = useState(100);
-  const [turn, setTurn] = useState(Player);
+  const [turn, setTurn] = useState("player");
 
   const attackEnemy = () => {
     setenemyHealth((prevhealth) => Math.max(0, prevhealth - 10));
-    setTurn("Enemy");
+    setTurn("enemy");
   };
 
   const attackPlayer = () => {
     setplayerHealth((prevhealth) => Math.max(0, prevhealth - 10));
-    setTurn("Player");
+    setTurn("player");
   };
 
   useEffect(() => {
-    if (turn === "Enemy" && playerhealth > 0 && enemyhealth > 0) {
+    if (turn === "enemy" && playerhealth > 0 && enemyhealth > 0) {
       setTimeout(()=>{attackPlayer();}, 1000);
     }
   }, [turn]
@@ -28,9 +28,9 @@ function App() {
     <div className="App">
       <h1>Button Fighter</h1>
         <p>Turn based fighter game. Player vs AI.</p>
-        <Player name={"Player"} health={playerhealth} />
-        <Enemy name={"Enemy"} health={enemyhealth} />
-        <button onClick={attackEnemy} disabled={turn !== "Player"}>Attack</button>
+        <Player name={"Hero"} health={playerhealth} />
+        <Enemy name={"Villen"} health={enemyhealth} />
+        <button onClick={attackEnemy} disabled={turn !== "player"}>Attack</button>
         {playerhealth === 0 && <h2> You Loss </h2>}
         {enemyhealth === 0 && <h2> You Win </h2>}
     </div>
