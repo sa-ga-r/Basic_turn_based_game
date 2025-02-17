1e907c29c78f900d8f9c00983e08d09c5cd246e4
sa-ga-r <116742231+sa-ga-r@users.noreply.github.com>
2025-02-17 12:30:08 +0000

    update

--- button_fighter/src/App.js
+++ button_fighter/src/App.js
@@ -1,11 +1,13 @@
+import Player from './components/player';
+import Enemy from './components/enemy';
 
 function App() {
   return (
     <div className="App">
       <h1>Button Fighter</h1>
-        <p>
-          Turn based fighter game. Player vs AI.
-        </p>
+        <p>Turn based fighter game. Player vs AI.</p>
+        <Player name={"Hero"} health={100} />
+        <Enemy name={"Villen"} health={100} />
     </div>
   );
 }
