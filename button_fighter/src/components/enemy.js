9c9ca9cc254d46258660392f21a102399b5ed3f4
sa-ga-r <116742231+sa-ga-r@users.noreply.github.com>
2025-02-17 13:01:49 +0000

    update

--- button_fighter/src/components/enemy.js
+++ button_fighter/src/components/enemy.js
@@ -1,7 +1,6 @@
-import react, {useState} from 'react';
+import react from 'react';
 
 function Enemy({name}){
-    const [health, setHealth] = useState(100);
     return (
         <div>
             <h2>{name}</h2>
