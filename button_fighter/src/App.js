176db503d83793940ecf96659cf4daa8ec362cf0
sa-ga-r <116742231+sa-ga-r@users.noreply.github.com>
2025-02-17 14:40:33 +0000

    update

--- button_fighter/src/App.js
+++ button_fighter/src/App.js
@@ -1,21 +1,38 @@
 import Player from './components/player';
 import Enemy from './components/enemy';
-import react, {useState} from 'react';
+import react, {useState, useEffect} from 'react';
 
 function App() {
   const [enemyhealth, setenemyHealth] = useState(100);
+  const [playerhealth, setplayerHealth] = useState(100);
+  const [turn, setTurn] = useState(Player);
 
   const attackEnemy = () => {
     setenemyHealth((prevhealth) => Math.max(0, prevhealth - 10));
+    setTurn("Enemy");
   };
+
+  const attackPlayer = () => {
+    setplayerHealth((prevhealth) => Math.max(0, prevhealth - 10));
+    setTurn("Player");
+  };
+
+  useEffect(() => {
+    if (turn === "Enemy" && playerhealth > 0 && enemyhealth > 0) {
+      setTimeout(()=>{attackPlayer();}, 1000);
+    }
+  }, [turn]
+  );
 
   return (
     <div className="App">
       <h1>Button Fighter</h1>
         <p>Turn based fighter game. Player vs AI.</p>
-        <Player name={"Hero"} />
+        <Player name={"Hero"} health={playerhealth} />
         <Enemy name={"Villen"} health={enemyhealth} />
-        <button onClick={attackEnemy}>Attack</button>
+        <button onClick={attackEnemy} disabled={turn !== "Player"}>Attack</button>
+        {playerhealth === 0 && <h2> You Loss </h2>}
+        {enemyhealth === 0 && <h2> You Win </h2>}
     </div>
   );
 }
