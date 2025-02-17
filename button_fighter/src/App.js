5471b10e30b03f7f021b749b8c45a08e24aa994e
sa-ga-r <116742231+sa-ga-r@users.noreply.github.com>
2025-02-17 13:58:04 +0000

    update

--- button_fighter/src/App.js
+++ button_fighter/src/App.js
@@ -3,12 +3,19 @@ import Enemy from './components/enemy';
 import react, {useState} from 'react';
 
 function App() {
+  const [enemyhealth, setenemyHealth] = useState(100);
+
+  const attackEnemy = () => {
+    setenemyHealth((prevhealth) => Math.max(0, prevhealth - 10));
+  };
+
   return (
     <div className="App">
       <h1>Button Fighter</h1>
         <p>Turn based fighter game. Player vs AI.</p>
         <Player name={"Hero"} />
-        <Enemy name={"Villen"} />
+        <Enemy name={"Villen"} health={enemyhealth} />
+        <button onClick={attackEnemy}>Attack</button>
     </div>
   );
 }
